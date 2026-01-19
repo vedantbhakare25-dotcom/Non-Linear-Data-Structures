b26c1b8eef87ef3009b601d844c70ad66002f6c4
vedantbhakare25-dotcom <vedant.bhakare25@vit.edu>
2026-01-19 11:09:13 +0530

    updated

---
+++ TREES/BInary Trees/DFS/PostOrder.cpp
@@ -0,0 +1,37 @@
+#include<bits/stdc++.h>
+using namespace std; 
+struct Node
+{
+    int data;
+    struct Node *left;
+    struct Node *right;
+    Node(int val)
+    {
+        data = val;
+        left = NULL;
+        right = NULL;
+    }
+};
+
+void  postorder(Node* root)
+{
+    if(root== NULL)
+    {
+        return;
+    }
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
+}
+
+int main()
+{
+    Node *root = new Node(1);
+    root->left=new Node(2);
+    root->right=new Node(3);
+    root->left->left=new Node(4);
+    root->left->right=new Node(5);
+    root->right->left=new Node(6);
+    postorder(root);
+    return 0;
+}
