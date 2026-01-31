06797cc84f65463d0675b35cc5a9f12d47c790cc
vedantbhakare25-dotcom <vedant.bhakare25@vit.edu>
2026-01-31 10:00:22 +0530

    updated

--- TREES/BInary Trees/DFS/PostOrder.cpp
+++ TREES/BInary Trees/DFS/PostOrder.cpp
@@ -13,17 +13,37 @@ struct Node
     }
 };
 
-void  postorder(Node* root)
-{
-    if(root== NULL)
-    {
-        return;
-    }
-    postorder(root->left);
-    postorder(root->right);
-    cout<<root->data<<" ";
-}
+// void  postorder(Node* root)
+// {
+//     if(root== NULL)
+//     {
+//         return;
+//     }
+//     postorder(root->left);
+//     postorder(root->right);
+//     cout<<root->data<<" ";
+// }
 
+vector<int> inorderTraversal(Node* root) {
+vector<int> result;
+
+if(root == NULL)
+    return result;
+
+vector<int> left = inorderTraversal(root->left);
+vector<int> right = inorderTraversal(root->right);
+
+// Left
+result.insert(result.end(), left.begin(), left.end());
+
+// Root
+result.push_back(root->data);
+
+// Right
+result.insert(result.end(), right.begin(), right.end());
+
+return result;
+}
 int main()
 {
     Node *root = new Node(1);
@@ -32,6 +52,10 @@ int main()
     root->left->left=new Node(4);
     root->left->right=new Node(5);
     root->right->left=new Node(6);
-    postorder(root);
+    vector<int> result = inorderTraversal(root);
+    for(int i=0;i<result.size();i++)
+    {
+        cout<<result[i]<<" ";
+    }   
     return 0;
 }
