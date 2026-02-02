6c78e352e713d4585a15072520ad40d10383b603
vedantbhakare25-dotcom <vedant.bhakare25@vit.edu>
2026-02-02 22:53:37 +0530

    updated

---
+++ TREES/Binary Search Tree/DeletingNode.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+struct treenode
+{
+    int data;
+    treenode *left;
+    treenode *right;
+    treenode(int val)
+    {
+        data=val;
+        left=right=NULL;
+    }
+};
+
+treenode* deletenode(treenode *root, int key)
+{
+    
+}
