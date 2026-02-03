0095137e290a2e354918c91cba3eb4d5bb48972d
vedantbhakare25-dotcom <vedant.bhakare25@vit.edu>
2026-02-03 19:11:03 +0530

    updated

--- TREES/Binary Search Tree/DeletingNode.cpp
+++ TREES/Binary Search Tree/DeletingNode.cpp
@@ -12,8 +12,88 @@ struct treenode
         left=right=NULL;
     }
 };
+treenode* findright(treenode *root)
+{
+    while(root->right != NULL)
+        root = root->right;
+    return root;
+}
+
+treenode* helper(treenode* root)
+{
+    if(root->left == NULL)
+    {
+        treenode* temp = root->right;
+        delete root;
+        return temp;
+    }
+    else if(root->right == NULL)
+    {
+        treenode* temp = root->left;
+        delete root;
+        return temp;
+    }
+
+    treenode* rightchild = root->right;
+    treenode* lastright = findright(root->left);
+    lastright->right = rightchild;
+
+    treenode* temp = root->left;
+    delete root;
+    return temp;
+}
 
 treenode* deletenode(treenode *root, int key)
 {
-    
+    if(root==NULL)
+    {
+        return NULL;
+    }
+    if(root->data==key)
+    {
+        return helper(root);
+    }
+    treenode *dummy=root;
+    while(root!=NULL)
+    {
+        if(root->data>key)
+        {
+            if(root->left!=NULL && root->left->data==key)
+            {
+                root->left=helper(root->left);
+            }
+            else
+            {
+                root=root->left;
+            }
+        }
+        else
+        {
+            if(root->right!=NULL && root->right->data==key)
+            {
+                root->right=helper(root->right);
+            }
+            else
+            {
+                root=root->right;
+            }
+        }
+    }
+    return dummy;
+}
+
+
+int main()
+{
+    treenode *root=new treenode(5);
+    root->left=new treenode(3);
+    root->right=new treenode(6);
+    root->left->left=new treenode(2);
+    root->left->right=new treenode(4);
+    root->right->right=new treenode(7);
+
+    int key=3;
+    treenode *newroot=deletenode(root,key);
+    // The tree now has the node with value 3 deleted.
+    return 0;
 }
